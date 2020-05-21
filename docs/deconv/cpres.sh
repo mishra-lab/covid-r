ebcda04337cfa490f1e970db781b785729dc1948
jessexknight <jesse.x.knight@gmail.com>
2020-05-20 22:19:27 -0400

    revise cpfig

---
+++ docs/deconv/cpres.sh
@@ -0,0 +1,13 @@
+# paths
+idir=../../out
+fidir=$idir/fig/.tmp/deconv
+vidir=$idir/value
+fodir=./fig
+vodir=./value
+# figures
+cp $fidir/deconv.pdf     $fodir/deconv.pdf
+cp $fidir/Re.pdf         $fodir/Re.pdf
+cp $fidir/Re-zoom.pdf    $fodir/Re-zoom.pdf
+cp $fidir/distr-refs.pdf $fodir/distr-refs.pdf
+# values
+cp $vidir/R-* $vodir/
